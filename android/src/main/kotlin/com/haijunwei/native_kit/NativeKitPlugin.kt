20606a803b9b638edff0fecd059ba772da4766a8
loper7 <loper7@163.com>
2021-07-07 14:00:57 +0800

    1

--- android/src/main/kotlin/com/haijunwei/native_kit/NativeKitPlugin.kt
+++ android/src/main/kotlin/com/haijunwei/native_kit/NativeKitPlugin.kt
@@ -1,6 +1,8 @@
 package com.haijunwei.native_kit
 
 import androidx.annotation.NonNull
+import com.haijunwei.native_kit.tool.AppInstaller
+import com.haijunwei.native_kit.tool.VolumeControl
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -9,7 +11,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** NativeKitPlugin */
-class NativeKitPlugin: FlutterPlugin, MethodCallHandler {
+class NativeKitPlugin: FlutterPlugin {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -18,18 +20,17 @@ class NativeKitPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "native_kit")
-    channel.setMethodCallHandler(this)
-  }
+    channel.setMethodCallHandler { call, result ->
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
     }
+
+    AppInstaller.instance.onAttachedToEngine(flutterPluginBinding)
+    VolumeControl.instance.onAttachedToEngine(flutterPluginBinding)
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+    AppInstaller.instance.onDetachedFromEngine(binding)
+    VolumeControl.instance.onDetachedFromEngine(binding)
   }
 }
